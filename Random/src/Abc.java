542793263c58ed23f2b8a4ad2fe1b0bdd405e2b3
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-06-03 08:49:44 +0530

    Add files via upload

---
+++ Random/src/Abc.java
@@ -0,0 +1,12 @@
+
+public class Abc {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+		System.out.println("Welcome To back Java World");
+
+	}
+
+}
