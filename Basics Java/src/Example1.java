3d7d2b7a6be07a2039f378d4c048da429accc3ee
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-11 08:06:48 +0530

    Update Example1.java

--- Basics Java/src/Example1.java
+++ Basics Java/src/Example1.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author 500062053
@@ -15,13 +9,13 @@ public class Example1
     public static void main(String args[])
     {
         int a,b,c;
-Scanner sc=new Scanner(System.in);
-a=Integer.parseInt(args[0]);
-System.out.println("number one is : "+a);
-b=Integer.parseInt(args[1]);
-System.out.println("number two is : "+b);
-c=a+b;
-System.out.println("Addition of two numbers is : "+c);
+        Scanner sc=new Scanner(System.in);
+        a=Integer.parseInt(args[0]);
+        System.out.println("number one is : "+a);
+        b=Integer.parseInt(args[1]);
+        System.out.println("number two is : "+b);
+        c=a+b;
+        System.out.println("Addition of two numbers is : "+c);
     }
     
 }
