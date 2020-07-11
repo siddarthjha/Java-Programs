a2dcdbaa10a4aba30bdab4e25592d02454067471
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-11 08:16:13 +0530

    Update NewClass.java

--- Basics Java/src/NewClass.java
+++ Basics Java/src/NewClass.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author 500062053
@@ -17,9 +11,5 @@ public class NewClass
     public static void main(String args[])
     {
         
-        
-        
     }
-    
-    
 }
