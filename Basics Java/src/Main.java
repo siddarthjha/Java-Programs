6b30d1c489e8ad088cd198f849c90b704c2beb65
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-11 08:13:02 +0530

    Update Main.java

--- Basics Java/src/Main.java
+++ Basics Java/src/Main.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Sid
@@ -12,14 +6,14 @@ public class Main
 {
     public static void main(String args[])
     {  
-final Customer c=new Customer();  
-new Thread(){  
-public void run(){c.withdraw(15000);}  
-}.start();  
-new Thread(){  
-public void run(){c.deposit(10000);}  
-}.start();  
+        final Customer c=new Customer();  
+        new Thread(){  
+        public void run(){c.withdraw(15000);}  
+        }.start();  
+        new Thread(){  
+        public void run(){c.deposit(10000);}  
+        }.start();  
   
-}
-    
+     }
+        
 }
