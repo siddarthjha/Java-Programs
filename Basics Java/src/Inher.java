83395f23a5a7edbbbd54159f3ef7004d54a624c7
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-11 08:11:05 +0530

    Update Inher.java

--- Basics Java/src/Inher.java
+++ Basics Java/src/Inher.java
@@ -1,17 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author 500062053
  */
 class Inher 
 {
-    
-    
     public static void main(String[] args) 
     {
          Inher obj=new Inher(); 
@@ -19,9 +11,6 @@ class Inher
          for(int j=0;j<100;j++)
          {
              System.out.println("j:"+j);
-         }
-    
-    }
-    
-    
+         }    
+    }    
 }
