25e97fe035be61b37575a493eff7e023803794b8
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-25 21:47:50 +0530

    Update Operators.java

--- Basics Java/src/Practice/Operators.java
+++ Basics Java/src/Practice/Operators.java
@@ -19,7 +19,18 @@ class Operators
 		System.out.println(x - y);
 		System.out.println(x / y);
 		System.out.println(x % y);
-		
+		System.out.println(x << 2);
+		System.out.println(x >> 3);
+		System.out.println(20 >>> 2);  //Works ">>>" same for positive numbers
+		System.out.println(-20 >>> 2); //Changes parity bit (MSB) to 0
+		System.out.println(x > y || x < y); // Logical Operator
+		System.out.println(x < y && x < y); // Logical Operator
+		System.out.println("x="+ x);
+		System.out.println("y="+ y);
+		System.out.println(x < y & x++ < y); // Bitwise Operator
+		System.out.println(x < y | x++ < y); // Bitwise Operator
+		int min = (x < y)? x:y;  
+		System.out.println(min); 
 		
 	}
 }
