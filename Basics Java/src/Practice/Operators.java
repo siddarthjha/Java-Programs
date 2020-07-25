24931e9596566520a1dcbad8983f8fd7c89f73bc
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-25 21:33:31 +0530

    Update Operators.java

--- Basics Java/src/Practice/Operators.java
+++ Basics Java/src/Practice/Operators.java
@@ -3,4 +3,23 @@
   TODO: Use of all Operators
 
 */
+class Operators
+{
+	public static void main(String[] args)
+	{
+		
+		int x = 10, y = 15;
+		System.out.println("x="+ x);
+		System.out.println("x++="+ x++);
+		System.out.println("++x="+ ++x);
+		System.out.println(x++ + ++x);
+		System.out.println("~x="+ ~x);
+		System.out.println(x+y);
+		System.out.println(x*y);
+		System.out.println(x-y);
+		System.out.println(x/y);
+		System.out.println(x%y);
+		
+	}
+}
 
