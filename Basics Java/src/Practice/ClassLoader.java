618ab051e21d6d11d5ed31ec39e11b9a6a78dfa2
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-25 19:13:25 +0530

    Update ClassLoader.java

--- Basics Java/src/Practice/ClassLoader.java
+++ Basics Java/src/Practice/ClassLoader.java
@@ -1,1 +1,11 @@
-
+class B
+{
+  // Class name can be different than the file name if we don't use public
+	public static void main(String[] args)
+	{
+		Class obj = B.class;
+		System.out.println(obj.getClassLoader());
+		System.out.println(String.class.getClassLoader());
+		
+	}
+}
