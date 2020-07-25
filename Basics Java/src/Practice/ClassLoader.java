245e726b6c9e7b1f50e4853b90802553382813c8
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-25 19:18:54 +0530

    Update ClassLoader.java

--- Basics Java/src/Practice/ClassLoader.java
+++ Basics Java/src/Practice/ClassLoader.java
@@ -6,6 +6,7 @@ class B
 		Class obj = B.class;
 		System.out.println(obj.getClassLoader());
 		// Output is java Class Loader
+		//Application/System classloader will load this class  
 		System.out.println(String.class.getClassLoader());
 		// This will produce null because "String" is in-built class.
 		//You can creaqte you own Class Loader by inheriting "ClassLoader" class.
