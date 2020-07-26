b1039a92471a2d169ad99498b7cb776d474a5845
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-26 10:13:03 +0530

    Update Super.java

--- Basics Java/src/Practice/Super.java
+++ Basics Java/src/Practice/Super.java
@@ -1,1 +1,53 @@
+/*
+  TODO: Understand the concept and use of super.
+ "Super cannot be used from static block".
+*/
+class Parent
+{
+	int a=10;
+	Parent()
+	{
+		System.out.println("Parent Constructor");
+	}
+	void parentMethod()
+	{
+		System.out.println("Parent Method");
+	}
+}
 
+class Child extends Parent
+{
+	Child()
+	{
+		// Implicit invoke of super method
+		// super();
+		System.out.println("Child Constructor");
+	}
+	void childMethod()
+	{
+		System.out.println("Super can access Parent class variable a="+super.a);
+		super.parentMethod();
+		// invoking parent class method
+		System.out.println("Child Method");
+	}
+}
+
+class Call extends Child
+{
+	public static void main(String[] args)
+	{
+		Call obj = new Call();
+		// implicit invoke of super 
+		// super();
+		obj.callMethod();
+		
+		
+	}
+	void callMethod()
+	{
+		super.childMethod();
+		// super can invoke parent class method
+		super.parentMethod();
+		System.out.println("The above one invoked Parent from call");
+	}
+}
