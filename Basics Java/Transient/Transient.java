d6f2e987792fa04f38d24ad35a2950eb6c394ec7
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-26 15:21:43 +0530

    Update Transient.java

--- Basics Java/Transient/Transient.java
+++ Basics Java/Transient/Transient.java
@@ -18,17 +18,23 @@ class Details implements Serializable
 public class Transient
 {
 	public static void main(String[] args)
-	{
+	{	
+		// Code for Serialization.
 		Details s1 =new Details("Siddarth", "Compiler", 95);//creating object  
   		//writing object into file  
   		FileOutputStream f=new FileOutputStream("output.txt");  
   		ObjectOutputStream out=new ObjectOutputStream(f);  
   		out.writeObject(s1);  
   		out.flush();  
-  
   		out.close();  
   		f.close();  
   		System.out.println("success");
+		// Code for Deserialization.
+		ObjectInputStream in=new ObjectInputStream(new FileInputStream("output.txt"));  
+  		Details s = (Details)in.readObject();  
+  		System.out.println(s.name+", "+s.subj+", "+s.marks);  
+  		in.close();  
+		System.out.println("Here we can see the marks are zero because the value was not serialized");
 		
 	}
 }
