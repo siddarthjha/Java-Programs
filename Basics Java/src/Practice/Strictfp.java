18ef2c40aeb656a49aedf27656d21bca5b3e95ca
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-26 10:40:11 +0530

    Update Strictfp.java

--- Basics Java/src/Practice/Strictfp.java
+++ Basics Java/src/Practice/Strictfp.java
@@ -1,1 +1,19 @@
-
+/*
+  TODO: Understand the concept of keyword "strictfp".
+*/
+public class Strictfp
+{	
+	static public void main(String[] args)
+	{
+		Strictfp obj = new Strictfp();
+		float c = obj.add(1.254f, 2.367f);
+		System.out.println(c);
+	}
+	strictfp float add(float a, float b) // This way the float values will not be changed even in different platforms
+	{
+		float c = a + b;
+		return c;
+		
+	}
+	
+}
