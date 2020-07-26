aafefbdeb585ad269e4a19d87086aff65327c518
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-26 15:14:18 +0530

    Update Transient.java

--- Basics Java/Transient/Transient.java
+++ Basics Java/Transient/Transient.java
@@ -1,1 +1,34 @@
+/*
+  TODO: To understand the concept of "transient" keyword.
+*/
+import java.io.Serializable;
+import java.io.*;
+class Details implements Serializable
+{
+	string name,subj;
+	transient int marks;
+	Details(string name, string subj, int marks)
+	{
+		this.name = name;
+		this.subj = subj;
+		this.marks = marks; 
+	}
+}
 
+public class Transient
+{
+	public static void main(String[] args)
+	{
+		Details s1 =new Details("Siddarth", "Compiler", 95);//creating object  
+  		//writing object into file  
+  		FileOutputStream f=new FileOutputStream("output.txt");  
+  		ObjectOutputStream out=new ObjectOutputStream(f);  
+  		out.writeObject(s1);  
+  		out.flush();  
+  
+  		out.close();  
+  		f.close();  
+  		System.out.println("success");
+		
+	}
+}
