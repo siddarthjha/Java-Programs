2eb109b9db20f99843defe491fe4a0f581fed620
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-27 16:35:01 +0530

    Update This.java

--- Basics Java/src/Practice/This.java
+++ Basics Java/src/Practice/This.java
@@ -1,1 +1,39 @@
-
+/*
+  TODO: Understand the concept of this keyword.
+*/
+public class This
+{
+	int a;
+	void fun()
+	{
+		
+		System.out.println("fun called by 'this' keyword");
+		n(this);
+		
+	}
+	public This()
+	{
+		System.out.println("We can call different constructor by this keyword");
+	}
+	public This(int a)
+	{
+		this();
+		this.a = a;
+		System.out.println("This can be used to intialize the instance variables if they have same name");
+		this.fun();
+	}
+	void n(This obj)
+	{	
+		System.out.println("this can also passed as an arguement (Used in event handling)");
+		System.out.println("And can also be passed to constructor");
+		
+	}
+	
+	public static void main(String args[])
+	{
+		This obj = new This(1);
+		obj.fun();
+		
+	}
+	
+}
