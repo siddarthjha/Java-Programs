90989e1d567bb465db15e5638f5e7453d24d73fd
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-27 18:36:01 +0530

    Update OOPS.java

--- Basics Java/src/Practice/OOPS.java
+++ Basics Java/src/Practice/OOPS.java
@@ -4,16 +4,21 @@
   2. Object
   3. Inheritance
   4. Constructor
-  5.
+  5. Overloading
+  6. Overriding.
 */
 class Dummy
 {
 	void ext()
 	{
-		System.out.println("I am grand parent function");
+		System.out.println("I am grand-parent function");
+	}
+	void ext(int a) // Overloading
+	{
+		System.out.println("I am overloaded grand-parent function");
 	}
 }
-public class OOPS extends Dummy
+public class OOPS extends Dummy // Inheritance
 {
 	int marks;
 	String subj, name; // Instance variable
@@ -33,18 +38,19 @@ public class OOPS extends Dummy
 		System.out.println("Subj = "+obj.subj+"\nName: "+obj.name);
 		Dummy obj3 = new Dummy();
 		obj3.ext();
+		obj3.ext(7);
 	}
 	void fun(int m, String n)
 	{
 		marks = m;
 		subj = n;
 	}
-	void ext()
+	void ext() // Overriding
 	{
 		System.out.println("I am parent function");
 	}
 }
-class Abc extends OOPS // inherited class OOPS
+class Abc extends OOPS // inherited class OOPS (Multi-level inheritance)
 {
 	public static void main(String[] args)
 	{
