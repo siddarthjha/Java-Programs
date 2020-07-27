0d4fe3b7d1e781db7efade9e4d9b15ae85f94a4d
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-27 18:22:34 +0530

    Update OOPS.java

--- Basics Java/src/Practice/OOPS.java
+++ Basics Java/src/Practice/OOPS.java
@@ -1,7 +1,19 @@
 /*
   TODO: Understand some OOPS concepts
+  1. Class
+  2. Object
+  3. Inheritance
+  4. Constructor
+  5.
 */
-public class OOPS
+class Dummy
+{
+	void ext()
+	{
+		System.out.println("I am grand parent function");
+	}
+}
+public class OOPS extends Dummy
 {
 	int marks;
 	String subj, name; // Instance variable
@@ -19,14 +31,20 @@ public class OOPS
 		OOPS obj1 = new OOPS(), obj2 = new OOPS(); // Creating multiple objects
 		System.out.println("Marks-"+obj.marks);
 		System.out.println("Subj = "+obj.subj+"\nName: "+obj.name);
+		Dummy obj3 = new Dummy();
+		obj3.ext();
 	}
 	void fun(int m, String n)
 	{
 		marks = m;
 		subj = n;
 	}
+	void ext()
+	{
+		System.out.println("I am parent function");
+	}
 }
-class A
+class Abc extends OOPS // inherited class OOPS
 {
 	public static void main(String[] args)
 	{
@@ -40,11 +58,12 @@ class A
 		System.out.println("Marks: "+obj.marks);
 		System.out.println("Subj: "+obj.subj+"\nName: "+obj.name);
 		System.out.println("Another name: "+obj1.name);
-		// Another way to initialize os bu method
+		// Another way to initialize object via method
 		obj1.fun(95, "Compiler");
 		System.out.println("Subj: "+obj1.subj+"\nMarks: "+obj1.marks);
 		new OOPS().fun(10, "Anonymous object");
-		// Annonymous object 		
+		// Annonymous object 
+		obj.ext();		
 		
 	} 
 }
