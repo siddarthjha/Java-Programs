0fcfb358cb360b6303899d622cc9babf4535a5fc
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-28 08:09:25 +0530

    Update Casting.java

--- Basics Java/src/Practice/Casting.java
+++ Basics Java/src/Practice/Casting.java
@@ -1,1 +1,30 @@
-
+/*
+  TODO: Understand concept of upcasting and downcasting.
+*/
+class Parent
+{
+	void dad()
+	{
+		System.out.println("I am dad function");
+	}
+	void m()
+	{
+		System.out.println("M function");
+	}
+}
+public class Casting extends Parent
+{
+	void dad()
+	{
+		System.out.println("I am child function");
+	}
+	public static void main(String[] args) throws Exception
+	{
+		Parent obj = new Casting(); // Upcasting
+		Casting obj1 = (Casting)obj; // Downcasting
+		obj.dad(); 
+		obj1.dad();
+		obj.m();
+		obj1.m();	
+	}
+}
