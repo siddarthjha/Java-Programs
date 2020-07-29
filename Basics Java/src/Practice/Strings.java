377116a33d6dc06f1e3b91f9c4398f059a937ad9
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-29 17:59:53 +0530

    Update Strings.java

--- Basics Java/src/Practice/Strings.java
+++ Basics Java/src/Practice/Strings.java
@@ -1,4 +1,32 @@
 /*
   TODO: Understand the concept of String.
 */  
+import java.util.*;
+public class Strings
+{
+	public static void main(String[] args)
+	{
+		// Strings are immutable
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter some input");
+		String s1 = sc.nextLine();
+		String s = "Siddarth";
+		System.out.println(s.equalsIgnoreCase(s1));
+		// StringBuffer class let's us build mutuable string.
+		StringBuffer s2 = new StringBuffer("Siddarth");
+		s2.insert(8," Kumar jha");
+		System.out.println("StringBuffer");
+		System.out.println(s2);
+		// StringBuilder also does the same but it is non-synchronized.
+		StringBuilder s3 = new StringBuilder("Siddarth");
+		s3.append(" Kumar jha ");
+		System.out.println(s3);
+		System.out.println("StringBuilder");
+		StringTokenizer s4 = new StringTokenizer("Hello i am Siddarth");
+		while (s4.hasMoreTokens()) 
+		{  
+         	System.out.println(s4.nextToken());  
+		}
+	}
+}
 
