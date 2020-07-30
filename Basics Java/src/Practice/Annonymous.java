769ffb0a49d7931a8ae55d04ded310848a94c7d5
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-30 15:45:58 +0530

    Update Annonymous.java

--- Basics Java/src/Practice/Annonymous.java
+++ Basics Java/src/Practice/Annonymous.java
@@ -1,1 +1,21 @@
-
+/*
+  TODO: Understand concept of annonymous class.
+*/
+interface It
+{  
+ void at();  
+}  
+	public class Annonymous
+	{  
+		 public static void main(String args[])
+		 {  
+			 It e=new It()
+			 {  
+			  	public void at()
+			  	{
+			  		System.out.println("Annonymous define");
+			  	}  
+			 };  
+		 	 e.at();  
+	     }  
+	}  
