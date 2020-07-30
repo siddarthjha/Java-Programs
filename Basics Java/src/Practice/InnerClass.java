0a82d75f6120b920ec18edb38a17fff59cad94b8
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-30 15:38:14 +0530

    Update InnerClass.java

--- Basics Java/src/Practice/InnerClass.java
+++ Basics Java/src/Practice/InnerClass.java
@@ -1,1 +1,19 @@
-
+/*
+  TODO: Understand the concept of Inner class.
+*/  
+public class InnerClass
+{
+	class Inner
+	{
+		void text()
+		{
+			System.out.println("Hello I am inner class text");
+		}
+	}
+	public static void main(String[] args)
+	{
+		InnerClass obj = new InnerClass();
+		InnerClass.Inner ob = obj.new Inner();
+		ob.text();
+	}
+}
