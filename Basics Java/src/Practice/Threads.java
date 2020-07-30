7554f7f0bd48d26c96255dfa35f45c8ff38648f4
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-30 17:34:44 +0530

    Update Threads.java

--- Basics Java/src/Practice/Threads.java
+++ Basics Java/src/Practice/Threads.java
@@ -1,1 +1,61 @@
+/*
+  TODO: Threads
+*/
+
+public class Threads extends Thread
+{
+	public void run()
+	{
+		System.out.println("Run is invoked by JVM after calling start()---Thread class");
+		for(int i = 0; i<=5; i++)
+		{
+			System.out.println(i);
+			try
+			{
+				Thread.sleep(10);
+			}
+			catch(Exception e)
+			{
+				System.out.println(e.getMessage());
+			}
+		}
+		System.out.println(Thread.currentThread().getName());
+	}
+	public static void main(String args[])
+	{
+		Threads obj = new Threads(), obj1 = new Threads(), obj2 = new Threads();
+		obj.start();
+		try
+		{
+			//obj.join();	
+			obj.join(100);
+		}
+		catch(Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+		obj1.start();
+		obj2.start();
+		
+		
+	}
+}
+
+class Threads1 implements Runnable
+{
+	public void run()
+	{
+		System.out.println("Run is invoked by JVM after calling start()---Runnable interface");
+	}
+	public static void main(String args[])
+	{
+		Threads ob = new Threads();
+		Thread obj = new Thread(ob);
+		obj.setDeamon(true);// Deamon thread
+		obj.start();
+		
+	}
+}
+
+
 
