dcf28ca53d755c57c4b0b5f23d6f868f8c3501f7
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-31 19:33:58 +0530

    Update Client.java

--- Basics Java/src/Practice/Networking/Client.java
+++ Basics Java/src/Practice/Networking/Client.java
@@ -1,3 +1,22 @@
 /*
   TODO: Create a client and send a meesage to server.
 */
+public class Client 
+{  
+	public static void main(String[] args) 
+	{  
+		try
+		{      
+			Socket s=new Socket("localhost",6666);  
+			DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+			dout.writeUTF("Hello I have connected to server via client");  
+			dout.flush();  
+			dout.close();  
+			s.close();  
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}  
+	}  
+}  
