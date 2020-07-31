19c4b8a729d9ebb0a4116bac2ffdb8a168ab7764
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-07-31 19:33:02 +0530

    Update Networking.java

--- Basics Java/src/Practice/Networking/Networking.java
+++ Basics Java/src/Practice/Networking/Networking.java
@@ -1,4 +1,17 @@
 /*
   TODO: Create a Server .
 */  
-
+import java.io.*;  
+import java.net.*;  
+public class Networking
+{
+	public static void main(String[] args) throws Exception
+	{
+		ServerSocket ss = new ServerSocket(6666);  
+		Socket s = ss.accept();//establishes connection and waits for the client   
+		DataInputStream dis = new DataInputStream(s.getInputStream());  
+		String  str = (String)dis.readUTF();  
+		System.out.println("message= "+str);  
+		ss.close();    
+	}
+}
