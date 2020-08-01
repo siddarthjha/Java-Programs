f18e465065b0b2814b0f159e388d391f1dc70537
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-01 18:41:48 +0530

    Update Arraylist.java

--- Basics Java/src/Practice/Collections/Arraylist.java
+++ Basics Java/src/Practice/Collections/Arraylist.java
@@ -1,1 +1,21 @@
-
+/*
+  TODO: ArrayList
+*/
+import java.util.*;
+public class Arraylist
+{
+	public static void main(String[] args)
+	{
+		ArrayList <String> list = new ArrayList<String>();
+		list.add("Sid");//Adding object in arraylist  
+		list.add("Kumar");  
+		list.add("Jha");  
+		list.add("Sid");  
+		Iterator itr=list.iterator();  
+		while(itr.hasNext())
+		{  
+			System.out.println(itr.next());
+		}	  
+		
+	}
+}
