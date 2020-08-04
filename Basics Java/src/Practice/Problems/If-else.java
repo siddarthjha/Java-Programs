288f14ea9be2614cd802eb9b3de3adbffdfff9dc
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-04 17:05:02 +0530

    Update If-else.java

--- Basics Java/src/Practice/Problems/If-else.java
+++ Basics Java/src/Practice/Problems/If-else.java
@@ -1,1 +1,31 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if(n%2 == 1 || n<=20 && n>=6)
+        {
+            System.out.println("Weird");
+        }
+        else if(n>=2 && n<=5 || n>20)
+        {
+            System.out.println("Not Weird");
+        }
+        
+
+        scanner.close();
+    }
+}
 
