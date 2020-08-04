07db4ee9f72eb1a27e8e39fc1e28d1f83bfdbaf1
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-04 17:08:23 +0530

    Update loops_2.java

--- Basics Java/src/Practice/Problems/loops_2.java
+++ Basics Java/src/Practice/Problems/loops_2.java
@@ -1,1 +1,35 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+ 
+public class Solution {
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        for(int i=0; i<length; i++)
+            {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int n = sc.nextInt();
+            
+            for(int j =0;j<n;j++) {
+                System.out.print(getValue(a,b,j)+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+    static int getValue(int a,int b, int n) {
+        
+        int sum = a;
+        for(int z=n;z>=0;z--) {
+            sum = sum + ((int) Math.pow(2,z))*b;
+        }
+        
+        return sum;
+    }
+}
 
