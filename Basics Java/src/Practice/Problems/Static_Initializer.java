5e907a25da3275ef99e6618fbb82bce172d1137a
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-04 17:11:51 +0530

    Update Static_Initializer.java

--- Basics Java/src/Practice/Problems/Static_Initializer.java
+++ Basics Java/src/Practice/Problems/Static_Initializer.java
@@ -1,1 +1,28 @@
 
+    static boolean flag = false;
+    static int B, H;
+    static
+    {
+        Scanner sc = new Scanner(System.in);
+        B = sc.nextInt();
+        H = sc.nextInt();
+        try
+        {
+            if(B <= 0 || H <= 0)
+            {
+                throw new Exception("Breadth and height must be positive");
+            }
+            else
+            {
+                flag = true;
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+
+
+
+
