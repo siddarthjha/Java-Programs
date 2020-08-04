2242edd0d3efd3c5a7de7a454781f298eac8fe13
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-04 17:10:33 +0530

    Update End_of_file.java

--- Basics Java/src/Practice/Problems/End_of_file.java
+++ Basics Java/src/Practice/Problems/End_of_file.java
@@ -1,1 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution 
+{
+
+    public static void main(String[] args) 
+    {
+         Scanner sc = new Scanner(System.in);
+        int line_num = 0;
+        while(sc.hasNext())
+            System.out.println(++line_num + " " + sc.nextLine());
+        sc.close();
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
 
