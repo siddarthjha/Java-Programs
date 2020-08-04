d2eadaf347a091d16085040af636e6e2b355ad0a
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-04 17:06:36 +0530

    Update std_in_out_2.java

--- Basics Java/src/Practice/Problems/std_in_out_2.java
+++ Basics Java/src/Practice/Problems/std_in_out_2.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 
-public class Solution {
+public class Solution 
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         Scanner scan = new Scanner(System.in);
         int i = scan.nextInt();
         double d = scan.nextDouble();
