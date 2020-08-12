b52de656b9a4205e240b2a96dc9cf1d294fa9c6a
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-12 17:21:46 +0530

    Update ObjectComparison.java

--- Basics Java/src/Practice/ObjectComparison.java
+++ Basics Java/src/Practice/ObjectComparison.java
@@ -1,1 +1,18 @@
-
+//import java.lang.Math;
+class Comparison
+{
+	public static void main(String[] args)
+	{
+		Double x = new Double(123.456), z = new Double(123.456);
+		
+		Long y = new Long(123456);
+		System.out.println(x == z);
+		System.out.println(x.equals(y));
+		System.out.println(y.equals(123456));
+		Integer a = x.hashCode();
+		Integer b = z.hashCode();
+		System.out.println(x.hashCode());
+		System.out.println(a.equals(b));
+		System.out.println((int)Math.random());
+	}
+}
