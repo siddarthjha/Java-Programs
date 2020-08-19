059e9be7ee23a4dae5036738011d1dd7ad76968d
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-19 22:10:04 +0530

    Update Sub_array.java

--- Basics Java/src/Practice/Problems/Sub_array.java
+++ Basics Java/src/Practice/Problems/Sub_array.java
@@ -1,1 +1,36 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
+public class Solution {
+
+    public static void main(String[] args) 
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [] ar = new int[n];
+        for(int i = 0; i < ar.length; i++)
+        {
+            ar[i] = sc.nextInt();
+        }
+        int counter = 0;
+        for(int  i = 0; i < ar.length; i++)
+        {
+            for(int j = i; j < ar.length; j++)
+            {
+                int sum = 0;
+                for(int k = i; k <= j; k++)
+                {
+                    sum += ar[k]; 
+                }
+                if(sum < 0)
+                    counter++;
+            }
+        }
+        sc.close();
+        System.out.println(counter);
+    }
+}
