0fa517b792599f3bff734083bc5608ea90d0119f
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-19 19:35:09 +0530

    Update Exception.java

--- Basics Java/src/Practice/Problems/Exception.java
+++ Basics Java/src/Practice/Problems/Exception.java
@@ -1,1 +1,34 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner obj = new Scanner(System.in);
+        
+        int a = 0, b = 0, c;
+        try
+        {
+            a = obj.nextInt();
+            b = obj.nextInt();
+            try
+            {
+                c = a/b;
+                System.out.println(c);
+            }
+        
+            catch(ArithmeticException e)
+            {
+                System.out.println("java.lang.ArithmeticException: / by zero");
+            }
+        }
+         catch(Exception e)
+        {
+            System.out.println("java.util.InputMismatchException");
+        }
+    }
+}
