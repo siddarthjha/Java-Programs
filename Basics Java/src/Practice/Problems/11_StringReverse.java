5c44fa0428b6cf45a486d5981ba4693f63d670a2
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-08-20 21:39:29 +0530

    Update 11_StringReverse.java

--- Basics Java/src/Practice/Problems/11_StringReverse.java
+++ Basics Java/src/Practice/Problems/11_StringReverse.java
@@ -1,1 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        char a[] = A.toCharArray();
+        /* Enter your code here. Print output to STDOUT. */
+        String result ="Yes";
+        int length = A.length();
+        for (int i=0; i<length/2;i++)
+        {
+            if(A.substring(i,i+1).equals(A.substring(length-i-1,length-i))) 
+            {         
+            }
+            else 
+            {
+             result ="No";
+            }
+        }
+        System.out.println(result);
+        
+    }
+}
+
+
+
 
