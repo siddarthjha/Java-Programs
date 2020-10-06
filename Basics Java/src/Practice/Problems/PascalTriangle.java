8823b2ed136451e8e997c9ba61550a84119792d3
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-10-06 12:17:36 +0530

    Update PascalTriangle.java

--- Basics Java/src/Practice/Problems/PascalTriangle.java
+++ Basics Java/src/Practice/Problems/PascalTriangle.java
@@ -1,1 +1,28 @@
-
+import java.util.Scanner;
+public class PascalTriangle
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		PascalTriangle obj = new PascalTriangle();
+		System.out.println("Enter the number");
+		int a = sc.nextInt();
+		obj.pascal(a);
+	}
+	
+	public void pascal(int a)
+	{
+		int c;
+		for(int line = 1; line <= a; line++)
+		{
+			c = 1;
+			for(int i = 1; i <= line; i++)
+			{
+				System.out.print(c + " ");
+				c = c * (line - i)/ i;
+			}
+			System.out.println();
+		}
+	}
+}
+		
