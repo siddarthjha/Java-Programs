1e4713e9b71d213f4e8b91d998dec12229cde0e1
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-10-06 12:24:53 +0530

    Update ArrayLeader.java

--- Basics Java/src/Practice/Problems/ArrayLeader.java
+++ Basics Java/src/Practice/Problems/ArrayLeader.java
@@ -1,1 +1,24 @@
-
+public class ArrayLeader
+{
+	public static void main(String[] args)
+	{
+		ArrayLeader obj = new ArrayLeader();
+		int arr[] = {10, 4, 7, 2, 6, 3, 1};
+		obj.leader(arr, arr.length);
+	}
+	
+	public void leader(int arr[], int size)
+	{
+		int max = arr[size - 1];
+		System.out.print(max + " ");
+		for(int i = size - 2; i >=0; i--)
+		{
+			if(max < arr[i])
+			{
+				System.out.print(arr[i] + " ");
+				max = arr[i];
+			}
+		}
+	}
+}
+	
