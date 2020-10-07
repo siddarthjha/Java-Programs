c0fbd6e330c3f2df6d624346e26972a21f41c9bb
Siddarth Kumar Jha <47585851+siddarthjha@users.noreply.github.com>
2020-10-07 19:44:55 +0530

    Update TwoSum.java

--- Basics Java/src/Practice/Problems/TwoSum.java
+++ Basics Java/src/Practice/Problems/TwoSum.java
@@ -1,1 +1,23 @@
-
+import java.util.*;
+class TwoSum
+{
+	public static void main(String[] args)
+	{
+		int ar[] = {1, 5, 9, 7, 3, 4};
+		int sum = 5;
+		Map<Integer, Integer> obj = new HashMap<>();
+		for(int i = 0; i < ar.length; i++)
+		{
+			obj.put(ar[i], i);
+		}
+		for(int i = 0; i < ar.length; i++)
+		{
+			int diff = sum - ar[i];
+			if(obj.containsKey(diff) && obj.get(diff) != i)
+			{
+				System.out.println(i + " " + obj.get(diff));
+				break;
+			}
+		}
+	}
+}
